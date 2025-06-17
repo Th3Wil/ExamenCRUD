3d85876c71bde74488f2b27d3d1786eb310b0182
Th3Wil <yosoywil.desing@gmail.com>
2025-06-17 17:48:19 -0600

    Agrego archivo de ejemplo de conexión a base de datos

---
+++ config/conexion.php
@@ -0,0 +1,22 @@
+<?php
+// conexion.example.php
+
+$host = 'localhost';
+$db   = 'nombre_de_tu_base';
+$user = 'usuario_db';
+$pass = 'contraseña_db';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    die('Error de conexión: ' . $e->getMessage());
+}
